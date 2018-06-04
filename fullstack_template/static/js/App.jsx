643eb39df891a317a3cd4bd25685c844205253ca
George Mgaloblishvili <gm2665@columbia.edu>
2018-06-03 23:25:21 -0400

    Mockup v1

--- fullstack_template/static/js/App.jsx
+++ fullstack_template/static/js/App.jsx
@@ -8,22 +8,9 @@ var $ = require('jquery');
 import HeaderBackgroundImage from '../images/header.jpg';
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    addHeaderImg() {
-        let headerBg = new Image();
-        headerBg.src = HeaderBackgroundImage;
-    }
-
-    render () {
-        return (
-            <PageHeader>
-                <div className='header-contents'>
-                {this.addHeaderImg()}
-                <Hello name='Rimini' />
-                </div>
-            </PageHeader>
-        );
-    }
+  render () {
+    return (
+      <Hello />
+    )
+  }
 }
--- fullstack_template/static/js/Hello.jsx
+++ fullstack_template/static/js/Hello.jsx
@@ -1,45 +1,144 @@
 import React from "react";
-import { Button, Grid, Row, Col } from "react-bootstrap";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+  import { Container, Row, Col } from 'reactstrap';
+  import {Line} from 'react-chartjs-2';
+  import {Bar} from 'react-chartjs-2';
 
 var $ = require('jquery');
 
+const bardata = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Data',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+};
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Data',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+};
+
 export default class Hello extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {greeting: 'Hello ' + this.props.name};
+  constructor(props) {
+  super(props);
 
-        // This binding is necessary to make `this` work in the callback
-        this.getPythonHello = this.getPythonHello.bind(this);
-    }
+  this.toggle = this.toggle.bind(this);
+  this.state = {
+    isOpen: false
+  };
+}
+toggle() {
+  this.setState({
+    isOpen: !this.state.isOpen
+  });
+}
 
-    personaliseGreeting(greeting) {
-        this.setState({greeting: greeting + ' ' + this.props.name + '!'});
-    }
+displayName: 'LineExample';
+displayName: 'BarExample';
 
-    getPythonHello() {
-        $.get(window.location.href + 'hello', (data) => {
-            console.log(data);
-            this.personaliseGreeting(data);
-        });
-    }
+render() {
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href=""><img src="https://www.seeklogo.net/wp-content/uploads/2016/11/rbc-logo-preview-400x400.png"  id="RBC_logo"/>FRED Tracker</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Indexes</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="localhost:5000">Download</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
 
-    render () {
-        return (
-            <Grid>
-                <Row>
-                <Col md={2}>
-                    <h1>{this.state.greeting}</h1>
-                    <hr/>
-                </Col>
-                </Row>
-                <Row>
-                <Col md={7} mdOffset={5}>
-                    <Button bsSize="large" bsStyle="danger" onClick={this.getPythonHello}>
-                    Say hello!
-                    </Button>
-                </Col>
-                </Row>
-            </Grid>
-        );
-    }
+      <Container>
+        <Row>
+          <Col className="Graph_block">
+            <Line data={data} />
+          </Col>
+          <Col className="Graph_block">
+            <Bar
+          data={bardata}
+          width={100}
+          height={50}
+          options={{
+            maintainAspectRatio: false
+          }}
+        />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="Graph_block">
+            <Line data={data} />
+          </Col>
+          <Col className="Graph_block">
+            <Line data={data} />
+          </Col>
+        </Row>
+      </Container>
+
+    </div>
+  );
+}
 }
