15ddd3646d553f5bf3c2364039668d8d320cd703
George Mgaloblishvili <gm2665@columbia.edu>
2018-06-06 16:39:01 -0400

    Bokeh proto

--- fullstack_template/server/server.py
+++ fullstack_template/server/server.py
@@ -1,65 +1,27 @@
 import random
-from bokeh.embed import components
-from bokeh.plotting import figure
-from bokeh.resources import INLINE
-from bokeh.util.string import encode_utf8
 from flask import Flask, render_template
+from bokeh.plotting import figure
+from bokeh.embed import components
 
 app = Flask(__name__, static_folder='../static/dist', template_folder='../static/client')
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def index(path):
-    return render_template('index.html')
+@app.route('/dashboard/')
+def show_dashboard():
+    plots = []
+    plots.append(make_plot())
 
-@app.route("/<int:bars_count>/")
-def chart(bars_count):
-    if bars_count <= 0:
-        bars_count = 1
-    return render_template("index.html", bars_count=bars_count)
+    return render_template('dashboard.html', plots=plots)
 
-@app.route('/')
-def bokeh():
+def make_plot():
+    plot = figure(plot_height=300, sizing_mode='scale_width')
 
-    # init a basic bar chart:
-    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
-    fig = figure(plot_width=600, plot_height=600)
-    fig.vbar(
-        x=[1, 2, 3, 4],
-        width=0.5,
-        bottom=0,
-        top=[1.7, 2.2, 4.6, 3.9],
-        color='navy'
-    )
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    y = [2**v for v in x]
 
-    # grab the static resources
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
+    plot.line(x, y, line_width=4)
 
-    # render template
-    script, div = components(fig)
-    html = render_template(
-        'index.html',
-        plot_script=script,
-        plot_div=div,
-        js_resources=js_resources,
-        css_resources=css_resources,
-    )
-    return encode_utf8(html)
-
-
-@app.route('/hello') # take note of this decorator syntax, it's a common pattern
-def hello():
-    # It is good practice to only call a function in your route end-point,
-    # rather than have actual implementation code here.
-    # This allows for easier unit and integration testing of your functions.
-    return get_hello()
-
-def get_hello():
-    greeting_list = ['Ciao', 'Hei', 'Salut', 'Hola', 'Hallo', 'Hej']
-    return random.choice(greeting_list)
-
-
+    script, div = components(plot)
+    return script, div
 
 if __name__ == '__main__':
     app.run(debug=True)
