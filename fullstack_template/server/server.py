fdf2fe4e38d387cf5942db6d65308ba4815b3c1a
George Mgaloblishvili <gm2665@columbia.edu>
2018-06-07 17:55:47 -0400

    Implemented Multi FRED Graph Functionality

--- fullstack_template/server/server.py
+++ fullstack_template/server/server.py
@@ -10,88 +10,16 @@ fr = Fred(api_key='9191e886eb8b7e932d92df410fbf0c9e',response_type='df')
 
 app = Flask(__name__, static_folder='../static/dist', template_folder='../static/client')
 
-def make_plot():
-    plot = figure(plot_height=250, sizing_mode='scale_width')
-
-    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    y = [2**v for v in x]
-
-    plot.line(x, y, line_width=4)
-
-    script, div = components(plot)
-    return script, div
-
-def make_plot2():
-    plot = figure(plot_height=250, sizing_mode='scale_width')
-
-    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    y = [2**v for v in x]
-
-    plot.line(x, y, line_width=4)
-
-    script, div = components(plot)
-    return script, div
-
-def make_plot3():
-    plot = figure(plot_height=250, sizing_mode='scale_width')
-
-    x = [0, 10, 25, 35, 42, 52, 66, 77, 88, 92]
-    y = [2**v for v in x]
-
-    plot.line(x, y, line_width=4)
-
-    script, div = components(plot)
-    return script, div
-
-def chart():
-
-    fruits = ['A', 'P', 'N', 'B', 'V', 'C', 'D']
-    plot = figure(x_range=fruits, plot_height=250, title="Company Performance", sizing_mode='scale_width')
-    plot.vbar(x=fruits, top=[5, 3, 4, 2, 4, 6], width=0.9)
-
-    plot.xgrid.grid_line_color = None
-    plot.y_range.start = 0
-
-    script, div = components(plot)
-    return script, div
-
-    #return render_template("chart.html",)
-
-x, y = 0, 0
-
-@app.route('/data/', methods=['POST'])
-def data():
-    global x, y
-    x += 1
-    y = x//1.3
-    return jsonify(x=[x], y=[y])
-
-def make_ajax_plot():
-    source = AjaxDataSource(data_url=request.url_root + 'data/',
-                            polling_interval=20000, mode='append')
-
-    source.data = dict(x=[], y=[])
-
-    plot = figure(x_range=[0,1000], y_range=[10, 2000], plot_height=250, sizing_mode='scale_width')
-    plot.line('x', 'y', source=source, line_width=4)
-
-    script, div = components(plot)
-    return script, div
+#Define Graph Data Source
+Urban_Index = fr.series.observations('CPIAUCSL')
+Real_GDP = fr.series.observations('A191RL1Q225SBEA')
 
 
-res = fr.series.observations('CPIAUCSL')
+#Create Graph
+def fred_plot1():
 
-#res1 = res['series_count']
-#print (res1)
-
-def fred_plot():
-
-#    x=[res['popularity']]
-#    y=[res['series_count']]
-
-
-    plot = figure(y_range=[0, 350], plot_height=250, x_axis_type='datetime', sizing_mode='scale_width')
-    plot.line(source=res, x='date', y='value',  line_width=4)
+    plot = figure(y_range=[0, 280], plot_height=300, x_axis_type='datetime', sizing_mode='scale_width')
+    plot.line(source=Urban_Index, x='date', y='value',  line_width=4)
 
     plot.xaxis.axis_label = "Year"
     plot.xaxis.axis_label_standoff = 10
@@ -102,20 +30,37 @@ def fred_plot():
 
     plot.add_layout(Title(text="Consumer Price Index for All Urban Consumers", align="center"), "above")
 
+    script, div = components(plot)
+    return script, div
 
+def fred_plot():
+
+    plot = figure(y_range=[-15, 20], plot_height=300, x_axis_type='datetime', sizing_mode='scale_width')
+    plot.line(source=Real_GDP, x='date', y='value',  line_width=2)
+
+    plot.xaxis.axis_label = "Year"
+    plot.xaxis.axis_label_standoff = 10
+    plot.xaxis.axis_label_text_font_style = "normal"
+    plot.yaxis.axis_label = "% Change from Preceding Period"
+    plot.xaxis.axis_label_standoff = 10
+    plot.yaxis.axis_label_text_font_style = "normal"
+
+    plot.add_layout(Title(text="Real GDP", align="center"), "above")
 
     script, div = components(plot)
     return script, div
 
+
+
+
+
+#Render Webpage#
 @app.route('/dashboard/')
 def show_dashboard():
     plots = []
+#Call Graph Function
     plots.append(fred_plot())
-    plots.append(make_ajax_plot())
-    plots.append(make_plot())
-    plots.append(make_plot2())
-    plots.append(make_plot3())
-    plots.append(chart())
+    plots.append(fred_plot1())
 
     return render_template('index.html', plots=plots)
 
